f2f45779f53ef7c180ed6e010330df89a58b949f
Kranti18kiran <nanikiri@gmail.com>
2024-07-26 08:49:20 +1000

    formatted

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-num1= 5
-num2 = 10
-sum_result = num1 + num2
-print(f"sum value of {num1} & {num2} is: {sum_result}")
+num1= 5;
+num2 = 10;
+
+#adding 2 values
+sum_result = num1 + num2;
+
+#_print summary_Sum
+    
+print(f"sum value of {num1} & {num2} is: {sum_result}");
 
  
